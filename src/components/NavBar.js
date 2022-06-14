44918d1be77c7626799919ace76ade95562f96fd
Nicolas Pressello <npressello@gmail.com>
2022-06-14 19:20:50 -0300

    agrego tailwindcss

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import logo from '../assets/images/shoes.png';
+
+function NavBar() {
+  return (
+    <header className="flex max-h-16 justify-between items-center">
+      <div className="flex items-center flex-shrink-0">
+        <img src={logo} className="object-contain h-16" alt="logo" />
+        <span className="font-semibold text-xl tracking-tight">Calzate</span>
+      </div>
+      <nav>
+        <ul className="flex justify-between">
+          <li><a href="" className="bg-blue-600">Inicio</a></li>
+          <li><a href="" className="bg-blue-600">Hombre</a></li>
+          <li><a href="" className="bg-blue-600">Mujer</a></li>
+          <li><a href="" className="bg-blue-600">Marcas</a></li>
+        </ul>
+      </nav>
+      <div>
+        <a href="">Cuenta</a>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
