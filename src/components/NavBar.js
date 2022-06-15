fbb2e9c1f8a8eea7f0d1b6c304b2f3bc755e8535
npressello <npressello@gmail.com>
2022-06-14 23:12:02 -0300

    Mejora de estilos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,23 @@ import logo from '../assets/images/shoes.png';
 
 function NavBar() {
   return (
-    <header className="flex max-h-16 justify-between items-center">
-      <div className="flex items-center flex-shrink-0">
-        <img src={logo} className="object-contain h-16" alt="logo" />
-        <span className="font-semibold text-xl tracking-tight">Calzate</span>
+    <header className="flex max-h-16 justify-between items-center mx-4 mt-2">
+      <div>
+        <a href="index.html" className="flex items-center flex-shrink-0">
+          <img src={logo} className="object-contain h-16" alt="logo" />
+          <span className="font-semibold text-xl tracking-tight mx-2">Calzate</span>
+        </a>
       </div>
       <nav>
         <ul className="flex justify-between">
-          <li><a href="#" className="border border-rose-600">Inicio</a></li>
-          <li><a href="#" className="border border-rose-600">Hombre</a></li>
-          <li><a href="#" className="border border-rose-600">Mujer</a></li>
-          <li><a href="#" className="border border-rose-600">Marcas</a></li>
+          <li><a href="index.html" className="border-b border-calzate-bg-primary mx-2 px-2 py-1 transition-all hover:border-b-4">Inicio</a></li>
+          <li><a href="#" className="border-b border-calzate-bg-primary mx-2 px-2 py-1 transition-all hover:border-b-4">Hombre</a></li>
+          <li><a href="#" className="border-b border-calzate-bg-primary mx-2 px-2 py-1 transition-all hover:border-b-4">Mujer</a></li>
+          <li><a href="#" className="border-b border-calzate-bg-primary mx-2 px-2 py-1 transition-all hover:border-b-4">Marcas</a></li>
         </ul>
       </nav>
       <div>
-        <a href="#">Cuenta</a>
+        <a href="#" className='bg-calzate-bg-primary text-calzate-clr-primary rounded-2xl mx-4 px-3 py-2 transition-all hover:bg-calzate-bg-secondary'>Cuenta</a>
       </div>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const { hasSelectionSupport } = require('@testing-library/user-event/dist/utils');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'calzate-bg-primary': 'hsl(22, 100%, 56%)',
+        'calzate-bg-secondary': 'hsl(22, 100%, 48%)',
+        'calzate-clr-primary': 'hsl(22, 100%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
