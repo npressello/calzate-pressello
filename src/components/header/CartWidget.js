cd1af090811ab372f4231eb2f79bdbfd8c7c712c
npressello <npressello@gmail.com>
2022-06-20 19:10:39 -0300

    Cart items counter visual added

--- src/components/header/CartWidget.js
+++ src/components/header/CartWidget.js
@@ -4,8 +4,9 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 const CartWidget = () => {
   return (
     <>
-      <button className='h-8 w-8 my-auto text-calzate-100 opacity-75 hover:opacity-100' id="cart-toggle">
-        <FontAwesomeIcon icon={ faCartShopping } />
+      <button className='relative h-8 w-8 my-auto text-calzate-100 opacity-75 hover:opacity-100' id="cart-toggle">
+        <FontAwesomeIcon className='text-xl' icon={ faCartShopping } />
+        <span className='absolute w-4 right-0 bottom-0 text-xs rounded-full bg-black text-calzate-900'>0</span>
       </button>          
     </>
   );
