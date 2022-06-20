57e8208361d5ca1a2ffcf542485d9ed530a8e0e3
npressello <npressello@gmail.com>
2022-06-20 19:33:11 -0300

    Add toggle funtionality to login popup button

--- src/components/header/LoginNavBar.js
+++ src/components/header/LoginNavBar.js
@@ -1,12 +1,16 @@
 import CartWidget from './CartWidget';
 
 const LoginNavBar = () => {
+  const toggleLoginPopup = () => {
+    document.getElementById("login-popup-div").classList.toggle("hidden");
+  }
+
   return (
     <>
         <div className='relative hidden ml-auto md:flex'>
           <CartWidget />
-          <a id='toggle-login-popup' href="#" className='bg-calzate-400 text-calzate-900 rounded-3xl mx-4 px-3 py-2 align-middle transition-all hover:bg-calzate-300 hover:shadow-md'>Cuenta</a>
-          <div className='hidden absolute rounded border border-calzate-700 p-2 top-16 right-0 bg-white flex flex-col shadow-md'> {/*colocar hidden al terminar de testear*/}
+          <a id='toggle-login-popup' onClick={toggleLoginPopup} href="#" className='bg-calzate-400 text-calzate-900 rounded-3xl mx-4 px-3 py-2 align-middle transition-all hover:bg-calzate-300 hover:shadow-md'>Cuenta</a>
+          <div id='login-popup-div' className='hidden absolute rounded border border-calzate-700 p-2 top-16 right-0 bg-white flex flex-col shadow-md'> {/*colocar hidden al terminar de testear*/}
             <label htmlFor="inputEmail">Correo electrónico:</label>
             <input className="rounded border border-calzate-400 focus:outline-calzate-300 focus:shadow-md mb-2" type="email" name="email" id="inputEmail" />
             <label htmlFor="inputPassword">Contraseña:</label>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ const { hasSelectionSupport } = require('@testing-library/user-event/dist/utils'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"    
   ],
   theme: {
     extend: {
