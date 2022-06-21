c988afa4f35c071c9b1b7fb5c7aed4353bedd394
npressello <npressello@gmail.com>
2022-06-20 21:11:10 -0300

    Added funtionality to the mobile Hamburguer button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,8 +5,18 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import NavBar from './NavBar';
 import LoginNavBar from './LoginNavBar';
 import CartWidget from './CartWidget';
+import React, { Component, useState } from 'react';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState("hidden");
+  
+  const toggleMobileNavbar = () => {
+    setIsOpen((prevValue) => {
+      if (prevValue === "hidden") return "block";
+      else return "hidden";
+    });
+  }
+
   return(
     <header className="shadow-md py-1 md:py-2">
       <div className='container px-4 mx-auto md:flex md:items-center'>
@@ -18,13 +28,13 @@ const Header = () => {
           </a>
           <div className='flex justify-between my-auto w-20 md:hidden'>
             <CartWidget />
-            <button className='h-8 w-8 border border-solid border-calzate-300 rounded text-calzate-200 opacity-75 hover:opacity-100' id="navbar-toggle">
+            <button onClick={toggleMobileNavbar} className='h-8 w-8 border border-solid border-calzate-300 rounded text-calzate-200 opacity-75 hover:opacity-100' id="navbar-toggle">
               <FontAwesomeIcon icon={ faBars } />
             </button>
           </div>
         </div>
 
-        <NavBar />
+        <NavBar mobileOpen={isOpen} />
         <LoginNavBar />
 
       </div>
--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
-const NavBar = () => {
+const NavBar = ({mobileOpen}) => {
+  console.log(mobileOpen);
   return (
     <>
-        <nav className='hidden md:flex md:ml-auto mt-6 md:mt-0' id='navbar-collapse'> {/*colocar hidden en lugar de flex al terminar de testear*/}
+        <nav className={`${mobileOpen} md:flex md:ml-auto mt-6 md:mt-0`} id='navbar-collapse'> {/*colocar hidden en lugar de flex al terminar de testear*/}
           <ul className="flex flex-col md:flex-row md:justify-between">
             <li className='mx-2'><a href="#" className="block md:inline bg-calzate-400 md:bg-white rounded md:rounded-none md:border-b-4 border-calzate-400 text-calzate-900 md:text-calzate-100 mx-2 px-2 py-1 font-medium">Inicio</a></li>
             <li className='mx-2'><a href="#" className="block md:inline rounded md:rounded-none hover:bg-calzate-700 md:hover:bg-white md:border-b border-calzate-400 text-calzate-100 mx-2 px-2 py-1 transition-all hover:md:border-b-4">Hombre</a></li>
