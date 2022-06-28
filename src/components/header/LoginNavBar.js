fcb77636a61618b4542a3d98837c469648672184
npressello <npressello@gmail.com>
2022-06-28 18:33:04 -0300

    Change the account login div to a form

--- src/components/header/LoginNavBar.js
+++ src/components/header/LoginNavBar.js
@@ -10,16 +10,16 @@ const LoginNavBar = (props) => {
         <div className='relative hidden ml-auto md:flex'>
           <CartWidget itemsInCart={props.itemsInCart} />
           <a id='toggle-login-popup' onClick={toggleLoginPopup} href="#" className='bg-calzate-400 text-calzate-900 rounded-3xl mx-4 px-3 py-2 align-middle transition-all hover:bg-calzate-300 hover:shadow-md'>Cuenta</a>
-          <div id='login-popup-div' className='hidden absolute rounded border border-calzate-700 p-2 top-16 right-0 bg-white flex flex-col shadow-md z-10'>
+          <form id='login-popup-div' className='hidden absolute rounded border border-calzate-700 p-2 top-16 right-0 bg-white flex flex-col shadow-md z-10'>
             <label htmlFor="inputEmail">Correo electrónico:</label>
             <input className="rounded border border-calzate-400 focus:outline-calzate-300 focus:shadow-md mb-2" type="email" name="email" id="inputEmail" />
             <label htmlFor="inputPassword">Contraseña:</label>
             <input className="rounded border border-calzate-400 focus:outline-calzate-300 focus:shadow-md mb-2" type="password" name="password" id="inputPassword" />
             <div className='flex my-2'>
-              <button id='btn-login-large' className="p-1 mx-auto rounded hover:bg-calzate-400 border-2 border-calzate-400 text-calzate-100 hover:text-calzate-800 hover:font-semibold">Ingresar</button>
-              <button id='btn-signup-large' className="p-1 mx-auto rounded bg-calzate-400 hover:bg-calzate-700 border border-calzate-400 hover:border-calzate-700 text-calzate-800 hover:text-calzate-100 font-semibold">Registrarse</button>
+              <button id='btn-login-large' type='submit' className="p-1 mx-auto rounded hover:bg-calzate-400 border-2 border-calzate-400 text-calzate-100 hover:text-calzate-800 hover:font-semibold">Ingresar</button>
+              <button id='btn-signup-large' type='submit' className="p-1 mx-auto rounded bg-calzate-400 hover:bg-calzate-700 border border-calzate-400 hover:border-calzate-700 text-calzate-800 hover:text-calzate-100 font-semibold">Registrarse</button>
             </div>            
-          </div>
+          </form>
         </div>
     </>
   );
