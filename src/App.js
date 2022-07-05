31754134a5fdba5d67ab60377ef6d1f6c3950f6e
npressello <npressello@gmail.com>
2022-07-05 14:53:34 -0300

    Added NotFound page component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import './App.css';
 import Header from './components/header/Header';
 import ItemListContainer from './components/Landing/itemList/ItemListContainer';
 import ItemDetailContainer from './components/Landing/itemDetail/ItemDetailContainer';
-import { BrowserRouter, Routes, Route, IndexRoute } from "react-router-dom";
+import NotFound from './components/Error/NotFound';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Header />
-      <Routes>        
-        <Route exact path="/" element={<ItemListContainer />} />
-        <Route path="/index.html" element={<ItemListContainer />} />
+      <Routes>  
+        <Route exact path="/" element={<ItemListContainer />} />          
         <Route exact path="/category/:categoryId" element={<ItemListContainer />} />
-        <Route exact path="/item/:id" element={<ItemDetailContainer />} />              
+        <Route exact path="/item/:id" element={<ItemDetailContainer />} />    
+        <Route path="*" element={<NotFound />} />       
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Error/NotFound.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+
+const NotFound = () => {
+
+  return(
+    <div className="container text-center">
+      <h1 className="mx-auto">PAGE NOT FOUND</h1>
+    </div>
+  );
+};
+
+export default NotFound;
