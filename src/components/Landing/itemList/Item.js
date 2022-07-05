96593e0f87bfea02cfc83027b64d95eed3a5a757
npressello <npressello@gmail.com>
2022-07-05 13:17:02 -0300

    Styled heart button of item

--- src/components/Landing/itemList/Item.js
+++ src/components/Landing/itemList/Item.js
@@ -29,7 +29,7 @@ const Item = ({item}) => {
   return (
     <div className="relative w-44 md:w-60 2xl:w-72 mt-4 mx-auto bg-white flex flex-col overflow-hidden group">
       <button className={`absolute w-10 h-10 rounded-full right-2 top-2 bg-calzate-900 hover:bg-calzate-800 z-10 ${heartClicked ? 'opacity-100' : 'opacity-50'}`} aria-label='favorite'>
-        <FontAwesomeIcon onClick={clickHeart} icon={heartClicked ? faHeart : regularHeart} className='align-middle h-8 w-8 text-calzate-300 z-10' />
+        <FontAwesomeIcon onClick={clickHeart} icon={heartClicked ? faHeart : regularHeart} className='align-middle h-5 w-5 text-calzate-300 z-10' />
       </button>
       <Link exact='true' to={`/item/${item.id}`} href="#">
         <div className="border-b border-black overflow-hidden">
