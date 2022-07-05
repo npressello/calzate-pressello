e410a05bf1e541081acc0242e500bd971fda5499
npressello <npressello@gmail.com>
2022-07-05 14:26:11 -0300

    Add item detail skeleton when loading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/header/Header';
 import ItemListContainer from './components/Landing/itemList/ItemListContainer';
 import ItemDetailContainer from './components/Landing/itemDetail/ItemDetailContainer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, IndexRoute } from "react-router-dom";
 
 function App() {
 
@@ -11,6 +11,7 @@ function App() {
       <Header />
       <Routes>        
         <Route exact path="/" element={<ItemListContainer />} />
+        <Route path="/index.html" element={<ItemListContainer />} />
         <Route exact path="/category/:categoryId" element={<ItemListContainer />} />
         <Route exact path="/item/:id" element={<ItemDetailContainer />} />              
       </Routes>
