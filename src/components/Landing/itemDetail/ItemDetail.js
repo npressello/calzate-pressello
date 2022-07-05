c512fc89498fcfe0ad90cf9c7d7af7c3dd43b5fe
npressello <npressello@gmail.com>
2022-07-05 13:34:39 -0300

    Styled imagen of product detail

--- src/components/Landing/itemDetail/ItemDetail.js
+++ src/components/Landing/itemDetail/ItemDetail.js
@@ -30,7 +30,7 @@ const ItemDetail = ({ item }) => {
 
   return(
     <div className="container font-roboto mx-auto my-10 flex flex-col md:flex-row">
-      <div className='ml-auto mr-auto md:mr-10 p-10 md:border md:border-neutral-400'>
+      <div className='ml-auto mr-auto md:mr-10 md:p-10 md:border md:border-neutral-400'>
         <img className='w-96 h-96 object-cover' src={ item.imgUrl } alt={ item.title } />
       </div>
       <div className='text-center md:text-left md:ml-2 md:mr-auto'>
