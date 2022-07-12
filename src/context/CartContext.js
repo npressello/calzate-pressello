2f0f70be3543c3820ed56161dcf15abd57249b11
npressello <npressello@gmail.com>
2022-07-12 19:39:53 -0300

    Implemented Provider functions

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -8,24 +8,29 @@ const CustomProvider = ({ children }) => {
 
   const [products, setProducts] = useState([]);
 
-  const addItem = (item, quantity) => {
-    if (isInCart(item.id)) return;
+  const addItem = (item, sizeIndex, colorIndex, quantity) => {
+    if (!isInCart(item.id)) return;
+    setProducts([...products, { 'item': item, 'size': sizeIndex, 'color': colorIndex, 'quantity': quantity }]);
   }
 
   const removeItem = (itemId) => {
-    return;
+    if (!isInCart(itemId)) return;
+    setProducts(products.filter((product) =>
+      product.item.id !== itemId));
   }
 
   const clear = () => {
-    return;
+    setProducts([]);
   }
 
   const isInCart = (itemId) => {
-    return;
+    return products.some((product) => product.item.id === itemId);
   }
 
   const getQuantity = () => {
-    return;
+    return products.reduce((acc, product) =>
+      acc + product.quantity, 0
+    );
   }
 
   return (
