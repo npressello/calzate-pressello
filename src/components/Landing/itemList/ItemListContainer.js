1094469501870a1e12c64206a6a59da35b8e8940
npressello <npressello@gmail.com>
2022-07-18 20:48:33 -0300

    fixed tailwind dependency

--- src/components/Landing/itemList/ItemListContainer.js
+++ src/components/Landing/itemList/ItemListContainer.js
@@ -4,12 +4,12 @@ import Hero from "./Hero";
 import ItemList from "./ItemList";
 import { db } from "../../firebase/Firebase";
 
-const ItemListContainer = (props) => {  
+const ItemListContainer = (props) => {
   const [loaded, setLoaded] = useState(false);
   const [selectedProducts, setSelectedProducts] = useState([]);
 
   const { categoryId } = useParams();
-  
+
   console.log(db);
 
   const getProductsByCategory = (data, catId) => {
@@ -20,49 +20,49 @@ const ItemListContainer = (props) => {
     return prods;
   }
 
-  const getProductsData = (id) => {    
+  const getProductsData = (id) => {
     let url = (id === undefined) ? 'products.json' : '../../products.json';
     fetch(url, {
-      headers: { 
+      headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
       }
     })
-    .then( (resp) => resp.json() )
-    .then( (data) => {       
-      data.forEach(element => {
-        element.imgUrl = images[element.imgUrl];
-      });
-      setSelectedProducts(getProductsByCategory(data, id));
-    })
-    .catch(err => console.log(err))
-    .finally(() => setLoaded(true))
+      .then((resp) => resp.json())
+      .then((data) => {
+        data.forEach(element => {
+          element.imgUrl = images[element.imgUrl];
+        });
+        setSelectedProducts(getProductsByCategory(data, id));
+      })
+      .catch(err => console.log(err))
+      .finally(() => setLoaded(true))
   };
 
   useEffect(() => {
     setLoaded(false);
     setTimeout(() => {
-      getProductsData(categoryId);      
-    }, 2000);  
+      getProductsData(categoryId);
+    }, 2000);
   }, [categoryId]);
 
   // Temporal way to load the dynamics urls of the products' images
   const importAll = (r) => {
     let img = {};
-    r.keys().map((item, index) => { img[item.replace('./', '').replace('.jpg', '')] = r(item); });
+    r.keys().map((item, index) => { img[item.replace('./', '').replace('.jpg', '').replace('.png', '')] = r(item); });
     return img;
   }
 
-  const images = importAll(require.context('../../../assets/images/products', false, /\.(png|jpe?g|svg)$/));  
+  const images = importAll(require.context('../../../assets/images/products', false, /\.(png|jpe?g|svg)$/));
 
-  return(
+  return (
     <>
       <Hero />
       <div className="container items-center mx-auto mt-10 text-center">
         <ItemList items={selectedProducts} loaded={loaded} />
       </div>
     </>
-  );  
+  );
 }
 
 export default ItemListContainer;
