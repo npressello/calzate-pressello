8b1d229937418603960a1c249a7a4e5e300bdd80
npressello <npressello@gmail.com>
2022-07-24 01:01:11 -0300

    added shadow to item

--- src/components/Landing/itemList/ItemList.js
+++ src/components/Landing/itemList/ItemList.js
@@ -4,11 +4,11 @@ import ItemSkeleton from "./ItemSkeleton";
 
 const ItemList = ({ items, loaded }) => {
 
-  return(
-    <div className="container mx-auto gap-x-0.5 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-      { loaded ?
-        items.map((prod, index) => <Item key={index} item={prod} />):
-        [...Array(10)].map((e, i) => <ItemSkeleton key={i} />)       
+  return (
+    <div className="container mx-auto gap-x-0.5 grid grid-cols-2  sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      {loaded ?
+        items.map((prod, index) => <Item key={index} item={prod} />) :
+        [...Array(10)].map((e, i) => <ItemSkeleton key={i} />)
       }
     </div>
   );
